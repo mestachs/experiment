bfc3f79f14e82abea1a1e6cd3e5d5d083f09097d
Stephan Mestach <stephan.mestach@8thcolor.com>
2015-07-08 22:03:48 +0200

    Use icons, start imposition tab.

--- material/init.js
+++ material/init.js
@@ -8,20 +8,20 @@ $( document ).ready(function() {
     });
 
 
+    function evaluate(){
+        var item = $(this);
+        var relatedItem = $("#" + item.attr("data-related-item")).parent();
 
-
-function evaluate(){
-    var item = $(this);
-    var relatedItem = $("#" + item.attr("data-related-item")).parent();
-
-    if(item.is(":checked")){
-        relatedItem.css('visibility', 'visible');
-    }else{
-        relatedItem.css('visibility', 'hidden');
+        if(item.is(":checked")){
+            relatedItem.css('visibility', 'visible');
+        }else{
+            relatedItem.css('visibility', 'hidden');
+        }
     }
-}
 
-$('input[type="checkbox"]').click(evaluate).each(evaluate);
+    $('input[type="checkbox"]').click(evaluate).each(evaluate);
+    $('input[type="radio"]').click(evaluate).each(evaluate);
 
+    $('select').not('.disabled').material_select();
 
 });
--- material/readme.md
+++ material/readme.md
@@ -1,5 +1,5 @@
 Try to port a [Walloon form](http://forms6.wallonie.be/formulaires/AttestationTCNA.pdf)
 to google inspired [material css](http://materializecss.com/) framework (not [material design lite](https://github.com/google/material-design-lite)).
 
-![How it looks like](https://raw.githubusercontent.com/mestachs/experiment/master/material/screenshot.png "How it looks like")
+![[How it looks like](http://mestachs.github.io/experiment/material/)](https://raw.githubusercontent.com/mestachs/experiment/master/material/screenshot.png "How it looks like")
 
